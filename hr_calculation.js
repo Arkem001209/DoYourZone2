d07c05a8688f478bcd18a7ea03812dd54e417182
Max Arkell <maxarkell@icloud.com>
2024-04-21 15:06:49 +0100

    change values

--- hr_calculation.js
+++ hr_calculation.js
@@ -1,8 +1,12 @@
 function hrcalc() {
-    var gains =document.getElementById("zone2progress").value
-    var minhr = document.getElementById("minhr").value
-    var maxhr = document.getElementById("maxhr").value
-    var hrworkout = document.getElementById("hrworkout").value
+    var gainsb =document.getElementById("zone2progress").value
+    var minhrbox = document.getElementById("minhr").value
+    var maxhrbox = document.getElementById("maxhr").value
+    var hrworkoutbox = document.getElementById("hrworkout").value
+
+    var minhr = minhrbox.value
+    var maxhr = maxhrbox.value
+    var hrworkout = hrworkoutbox.value
     if (hrworkout.value <= maxhr.value) and (hrworkout.vale >=minhr.value); {
         gains.value = gains.value + 10
     }
